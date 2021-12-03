dbbd0a75d386cfcb21c4c0113f4ec45247201d22
Chris Reichert <chris.a.reichert@gmail.com>
2021-12-02 21:40:06 -0600

    got 14 of the tests to pass

--- pythonic_garage_band_cli/band.py
+++ pythonic_garage_band_cli/band.py
@@ -1,18 +1,68 @@
 
 
+from re import M
+
+
 class Band():
+    def __init__(self, name='unknown', members=[]):
+        self.name = name
+        self.members = members
+
+    def play_solos(self):
+        return self.members
+        
+
+
+class Musician(Band):
     pass
 
-class Musician():
-    pass
 
-class Guitarist():
-    pass
+class Guitarist(Musician):
+    def __init__(self, name='unknown'):
+        self.name = name
 
-class Bassist():
-    pass
+    def __str__(self):
+        return f'My name is {self.name} and I play guitar'
 
-class Drummer():
-    pass
+    def __repr__(self):
+        return f'Guitarist instance. Name = {self.name}'
+
+    def get_instrument(self):
+        return 'guitar'
+
+    def play_solo(self):
+        return 'face melting guitar solo'
 
 
+class Bassist(Musician):
+    def __init__(self, name='unknown'):
+        self.name = name
+
+    def __str__(self):
+        return f'My name is {self.name} and I play bass'
+
+    def __repr__(self):
+        return f'Bassist instance. Name = {self.name}'
+
+    def get_instrument(self):
+        return 'bass'
+
+    def play_solo(self):
+        return 'bom bom buh bom'
+
+
+class Drummer(Musician):
+    def __init__(self, name='unknown'):
+        self.name = name
+
+    def __str__(self):
+        return f'My name is {self.name} and I play drums'
+
+    def __repr__(self):
+        return f'Drummer instance. Name = {self.name}'
+
+    def get_instrument(self):
+        return 'drums'
+
+    def play_solo(self):
+        return 'rattle boom crash'
